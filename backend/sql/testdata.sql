051068b6aa4c97063a4ccff30ee5b67b1e9a33fd
Alexander Gabriel <alex.barbalex@gmail.com>
2021-04-19 22:17:04 +0200

    rename rev fields

--- backend/sql/testdata.sql
+++ backend/sql/testdata.sql
@@ -26,13 +26,13 @@ INSERT INTO fields (id, table_id, name, label, field_type, widget_type, client_r
 INSERT INTO row_revs (row_id, table_id, data, client_rev_by)
   VALUES ('a5aeaa00-8250-11eb-8dcd-0242ac130003', '961c4cba-824f-11eb-8dcd-0242ac130003', '{"field_1": "test in field 1", "field_2": "test in field 2"}', 'a1830c80-824d-11eb-8dcd-0242ac130003');
 
-INSERT INTO row_revs (row_id, table_id, data, client_rev_by, _rev, _revisions, _depth)
+INSERT INTO row_revs (row_id, table_id, data, client_rev_by, rev, revisions, depth)
   VALUES ('a5aeaa00-8250-11eb-8dcd-0242ac130003', '961c4cba-824f-11eb-8dcd-0242ac130003', '{"field_1": "test in field 1, verändert", "field_2": "test in field 2"}', 'a1830c80-824d-11eb-8dcd-0242ac130003', '1-1602ce5d8b68fd0172ef11a2598eedfe', '{"1-1602ce5d8b68fd0172ef11a2598eedfe"}', 1);
 
 INSERT INTO file_revs (file_id, row_id, field_id, filename, hash, version, client_rev_by)
   VALUES ('93c4dac0-8274-11eb-8dcd-0242ac130003', 'a5aeaa00-8250-11eb-8dcd-0242ac130003', 'ed1bd6c0-824f-11eb-8dcd-0242ac130003', 'test-filename', 'test-hash', 1, 'a1830c80-824d-11eb-8dcd-0242ac130003');
 
-INSERT INTO file_revs (file_id, row_id, field_id, filename, hash, version, client_rev_by, _rev, _revisions, _depth)
+INSERT INTO file_revs (file_id, row_id, field_id, filename, hash, version, client_rev_by, rev, revisions, depth)
   VALUES ('93c4dac0-8274-11eb-8dcd-0242ac130003', 'a5aeaa00-8250-11eb-8dcd-0242ac130003', 'ed1bd6c0-824f-11eb-8dcd-0242ac130003', 'test-filename changed', 'test-hash', 1, 'a1830c80-824d-11eb-8dcd-0242ac130003', '1-c407553f4c296c44ddd2b8c24ef847a7
 ', '{"1-c407553f4c296c44ddd2b8c24ef847a7"}', 1);
 
