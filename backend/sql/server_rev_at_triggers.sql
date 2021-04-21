88a11a986ed4e4c2e1c8b680d0ab7894192d49b1
Alexander Gabriel <alex.barbalex@gmail.com>
2021-04-21 17:54:04 +0200

    correct triggers

--- backend/sql/server_rev_at_triggers.sql
+++ backend/sql/server_rev_at_triggers.sql
@@ -22,7 +22,7 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_accounts_set_server_rev_at
-  after update on users
+  after update on accounts
   for each row
   execute procedure accounts_set_server_rev_at ();
 
@@ -36,7 +36,7 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_projects_set_server_rev_at
-  after update on users
+  after update on projects
   for each row
   execute procedure projects_set_server_rev_at ();
 
@@ -50,7 +50,7 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_rel_types_set_server_rev_at
-  after update on users
+  after update on rel_types
   for each row
   execute procedure rel_types_set_server_rev_at ();
 
@@ -64,7 +64,7 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_tables_set_server_rev_at
-  after update on users
+  after update on tables
   for each row
   execute procedure tables_set_server_rev_at ();
 
@@ -78,7 +78,7 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_field_types_set_server_rev_at
-  after update on users
+  after update on field_types
   for each row
   execute procedure field_types_set_server_rev_at ();
 
@@ -92,7 +92,7 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_widget_types_set_server_rev_at
-  after update on users
+  after update on widget_types
   for each row
   execute procedure widget_types_set_server_rev_at ();
 
@@ -106,7 +106,7 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_fields_set_server_rev_at
-  after update on users
+  after update on fields
   for each row
   execute procedure fields_set_server_rev_at ();
 
@@ -120,7 +120,7 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_role_types_set_server_rev_at
-  after update on users
+  after update on role_types
   for each row
   execute procedure role_types_set_server_rev_at ();
 
@@ -134,7 +134,7 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_project_users_set_server_rev_at
-  after update on users
+  after update on project_users
   for each row
   execute procedure project_users_set_server_rev_at ();
 
@@ -148,7 +148,7 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_version_types_set_server_rev_at
-  after update on users
+  after update on version_types
   for each row
   execute procedure version_types_set_server_rev_at ();
 
@@ -162,7 +162,7 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_news_set_server_rev_at
-  after update on users
+  after update on news
   for each row
   execute procedure news_set_server_rev_at ();
 
@@ -176,6 +176,6 @@ end;
 $$ language plpgsql;
 
 create trigger trigger_news_delivery_set_server_rev_at
-  after update on users
+  after update on news_delivery
   for each row
   execute procedure news_delivery_set_server_rev_at ();
