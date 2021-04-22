181534c989c5cf62a8aa11c0eb721bf7aae88405
Alexander Gabriel <alex.barbalex@gmail.com>
2021-04-22 14:01:50 +0200

    refactor triggers

--- backend/sql/server_rev_at_triggers.sql
+++ backend/sql/server_rev_at_triggers.sql
@@ -24,62 +24,70 @@ create trigger projects_set_server_rev_at
   for each row
   execute procedure set_server_rev_at ();
 
+
+
+
+
+
+
+
+
 -- rel_types
 create trigger rel_types_set_server_rev_at
-  after update on rel_types
+  before insert or update on rel_types
   for each row
   execute procedure set_server_rev_at ();
 
 -- tables
 create trigger tables_set_server_rev_at
-  after update on tables
+  before insert or update on tables
   for each row
   execute procedure set_server_rev_at ();
 
 -- field_types
 create trigger field_types_set_server_rev_at
-  after update on field_types
+  before insert or update on field_types
   for each row
   execute procedure set_server_rev_at ();
 
 -- widget_types
 create trigger widget_types_set_server_rev_at
-  after update on widget_types
+  before insert or update on widget_types
   for each row
   execute procedure set_server_rev_at ();
 
 -- fields
 create trigger fields_set_server_rev_at
-  after update on fields
+  before insert or update on fields
   for each row
   execute procedure set_server_rev_at ();
 
 -- role_types
 create trigger role_types_set_server_rev_at
-  after update on role_types
+  before insert or update on role_types
   for each row
   execute procedure set_server_rev_at ();
 
 -- project_users
 create trigger project_users_set_server_rev_at
-  after update on project_users
+  before insert or update on project_users
   for each row
   execute procedure set_server_rev_at ();
 
 -- version_types
 create trigger version_types_set_server_rev_at
-  after update on version_types
+  before insert or update on version_types
   for each row
   execute procedure set_server_rev_at ();
 
 -- news
-create trigger tnews_set_server_rev_at
-  after update on news
+create trigger news_set_server_rev_at
+  before insert or update on news
   for each row
   execute procedure set_server_rev_at ();
 
 -- news_delivery
 create trigger news_delivery_set_server_rev_at
-  after update on news_delivery
+  before insert or update on news_delivery
   for each row
   execute procedure set_server_rev_at ();
