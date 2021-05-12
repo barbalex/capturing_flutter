63abce37b027eacf956361d830e7c99bb26550cc
Alexander Gabriel <alex.barbalex@gmail.com>
2021-05-12 09:23:47 +0200

    work on files

--- lib/screens/row/file.dart
+++ lib/screens/row/file.dart
@@ -48,8 +48,60 @@ class _FileWidgetState extends State<FileWidget> {
             );
           } else {
             List<Cfile> files = snapshot.data;
+            print('file. files: $files, length: ${files.length}');
+            if (files.length == 0) {
+              return Text('files');
+            }
 
-            return Text('file picker');
+            return ListView.separated(
+              separatorBuilder: (BuildContext context, int index) => Divider(
+                color: Theme.of(context).primaryColor.withOpacity(0.5),
+                height: 1,
+              ),
+              itemBuilder: (context, index) {
+                Cfile file = files[index];
+                return Dismissible(
+                  key: Key(file.isarId.toString()),
+                  // Show a red background as the item is swiped away.
+                  background: Container(
+                    color: Theme.of(context).accentColor,
+                    child: Center(
+                      child: Text(
+                        'dismissing',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                  onDismissed: (direction) {
+                    file.delete();
+                    // Show a snackbar. This snackbar could also contain "Undo" actions.
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("${file.filename ?? ''} dismissed"),
+                      ),
+                    );
+                  },
+                  child: ListTile(
+                    title: Text(
+                      file.filename ?? '',
+                    ),
+                    onTap: () {
+                      print('TODO: open file');
+                    },
+                  ),
+                );
+              },
+              itemCount: files.length,
+              padding: EdgeInsets.symmetric(
+                vertical: 0,
+                horizontal: 0,
+              ),
+            );
+            ;
           }
         }
         return CircularProgressIndicator();
