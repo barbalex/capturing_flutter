350168a7ce6c33212bc3c2be9ed47baeb0c652ca
Alexander Gabriel <alex.barbalex@gmail.com>
2021-05-22 11:22:52 +0200

    optimize

--- lib/screens/project/project.dart
+++ lib/screens/project/project.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:isar/isar.dart';
 import 'package:capturing/models/project.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:recase/recase.dart';
 
 class ProjectWidget extends StatefulWidget {
   final List<Project> projects;
@@ -17,19 +18,14 @@ class _ProjectWidgetState extends State<ProjectWidget> {
   final Isar isar = Get.find<Isar>();
   final String id = Get.parameters['projectId'] ?? '';
   final name = ''.obs;
-  final nameErrorText = ''.obs;
   final label = ''.obs;
   final labelErrorText = ''.obs;
 
   @override
   Widget build(BuildContext context) {
     Project project = widget.projects.where((p) => p.id == id).first;
-    ever(nameErrorText, (_) {
-      setState(() {});
-    });
-    ever(labelErrorText, (_) {
-      setState(() {});
-    });
+    TextEditingController nameController = TextEditingController();
+    nameController.text = project.name ?? '';
 
     return Center(
       child: ListView(
@@ -38,63 +34,46 @@ class _ProjectWidgetState extends State<ProjectWidget> {
         children: <Widget>[
           Focus(
             onFocusChange: (hasFocus) async {
-              if (!hasFocus && name.value != project.name) {
-                try {
-                  project.name = name.value;
-                  project.save();
-                  nameErrorText.value = '';
-                } catch (e) {
-                  print(e);
-                  String errorText = e.toString();
-                  if (errorText.contains('Unique index violated')) {
-                    errorText = 'The name has to be unique';
-                  }
-                  nameErrorText.value = errorText;
-                }
-              }
-            },
-            child: FormBuilderTextField(
-              name: 'name',
-              decoration: InputDecoration(labelText: 'Name'),
-              onChanged: (String? val) {
-                name.value = val == null ? '' : val;
-              },
-              validator: FormBuilderValidators.compose([
-                (_) => nameErrorText.value != '' ? nameErrorText.value : null,
-                FormBuilderValidators.required(context),
-              ]),
-              keyboardType: TextInputType.emailAddress,
-              initialValue: project.name ?? '',
-            ),
-          ),
-          SizedBox(
-            height: 8.0,
-          ),
-          Focus(
-            onFocusChange: (hasFocus) async {
               if (!hasFocus && label.value != project.label) {
                 try {
-                  project.label = label.value;
-                  project.save();
-                  labelErrorText.value = '';
+                  project.label = label.value == '' ? null : label.value;
+                  name.value =
+                      label.value == '' ? '' : ReCase(label.value).snakeCase;
+                  project.name = name.value == '' ? null : name.value;
+                  await project.save();
+                  if (labelErrorText.value != '') {
+                    labelErrorText.value = '';
+                  }
                 } catch (e) {
                   labelErrorText.value = e.toString();
                 }
+                setState(() {});
               }
             },
             child: FormBuilderTextField(
               name: 'label',
-              decoration: InputDecoration(labelText: 'Label'),
+              decoration: InputDecoration(labelText: 'Name (user-friendly)'),
               onChanged: (String? val) {
-                label.value = val == null ? '' : val;
+                label.value = val ?? '';
               },
               validator: FormBuilderValidators.compose([
                 (_) => labelErrorText.value != '' ? labelErrorText.value : null,
                 FormBuilderValidators.required(context),
               ]),
-              keyboardType: TextInputType.emailAddress,
+              keyboardType: TextInputType.text,
               initialValue: project.label ?? '',
             ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          TextField(
+            controller: nameController,
+            decoration: InputDecoration(
+              labelText: 'Database-friendly Name (set automatically)',
+            ),
+            readOnly: true,
+            enabled: false,
           ),
         ],
       ),
