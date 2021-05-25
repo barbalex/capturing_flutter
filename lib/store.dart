47338f4277398022a896224dc0c12894f3fb371b
Alexander Gabriel <alex.barbalex@gmail.com>
2021-05-25 11:23:53 +0200

    check existance of url element called

--- lib/store.dart
+++ lib/store.dart
@@ -6,9 +6,11 @@ final activeUserEmail = ''.obs;
 // navigation is managed via this url
 // using Get.toNamed
 final url = <String>[].obs;
-String? get activeProjectId => url[1];
+// ensure not to tap a url element that does not exist
+// so always check url.length
+String? get activeProjectId => url.length > 1 ? url[1] : null;
 int get activeTableLevelCount => url.where((e) => e == '/tables/').length;
-String? get activeTableId1 => url[3];
+String? get activeTableId1 => activeTableLevelCount > 0 ? url[3] : null;
 String? get activeTableId2 => activeTableLevelCount > 1 ? url[5] : null;
 String? get activeTableId3 => activeTableLevelCount > 2 ? url[7] : null;
 String? get activeTableId4 => activeTableLevelCount > 3 ? url[9] : null;
@@ -22,5 +24,9 @@ String? get parentTableId {
   return null;
 }
 
-String? get activeRowId => url[url.indexOf('/rows/') + 1];
-String? get activeFieldId => url[url.indexOf('/fields/') + 1];
+String? get activeRowId => (url.indexOf('/rows/') + 1) < url.length
+    ? url[url.indexOf('/rows/') + 1]
+    : null;
+String? get activeFieldId => (url.indexOf('/fields/') + 1) < url.length
+    ? url[url.indexOf('/fields/') + 1]
+    : null;
