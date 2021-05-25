f0fa68f8c820930199b8eecb20362e4d8cabb114
Alexander Gabriel <alex.barbalex@gmail.com>
2021-05-25 11:24:46 +0200

    if only one table, navigate directly to rows

--- lib/screens/projects/tile.dart
+++ lib/screens/projects/tile.dart
@@ -1,6 +1,8 @@
 import 'package:capturing/models/project.dart';
+import 'package:capturing/models/table.dart';
 import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
+import 'package:capturing/isar.g.dart';
 import 'package:get/get.dart';
 import 'package:capturing/store.dart';
 
@@ -59,8 +61,35 @@ class ProjectTile extends StatelessWidget {
                 : Theme.of(context).primaryColor,
           ),
         ),
-        onTap: () {
-          // TODO: if only one table exists, navigate to rows list
+        onTap: () async {
+          List<Ctable> tables = await isar.ctables
+              .where()
+              .filter()
+              .projectIdEqualTo(project.id)
+              .and()
+              .optional(parentTableId == null, (q) => q.parentIdIsNull())
+              .and()
+              .optional(parentTableId != null,
+                  (q) => q.parentIdEqualTo(parentTableId))
+              .and()
+              .deletedEqualTo(false)
+              .and()
+              // show option tables only when editing structure
+              .optional(editingProject.value != project.id,
+                  (q) => q.optionTypeEqualTo(null))
+              .findAll();
+          int tableCount = tables.length;
+          // if only one table exists, navigate to rows list
+          if (tableCount == 1 && editingProject.value != project.id) {
+            url.value = [
+              '/projects/',
+              project.id,
+              '/tables/',
+              tables[0].id,
+              '/rows/',
+            ];
+            return;
+          }
           editingProject.value == project.id
               ? url.value = ['/projects/', project.id]
               : url.value = ['/projects/', project.id, '/tables/'];
