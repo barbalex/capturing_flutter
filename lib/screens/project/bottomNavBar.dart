31777a8916a8756f9e8a15be4fa45e8277f931bb
Alexander Gabriel <alex.barbalex@gmail.com>
2021-05-27 11:17:09 +0200

    remove horizontal navs

--- lib/screens/project/bottomNavBar.dart
+++ lib/screens/project/bottomNavBar.dart
@@ -1,108 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:capturing/models/project.dart';
 import 'package:capturing/store.dart';
 
 class BottomNavBar extends StatelessWidget {
   final List<Project> projects;
-  final int activePageIndex;
-  final PageController controller;
 
   BottomNavBar({
     required this.projects,
-    required this.activePageIndex,
-    required this.controller,
   });
 
   final String id = activeProjectId ?? '';
-  final RxInt bottomBarIndex = 0.obs;
 
   @override
   Widget build(BuildContext context) {
     Project project = projects.where((p) => p.id == id).first;
-    bool existsNextProject = projects.length > activePageIndex + 1;
-    bool existsPreviousProject = activePageIndex > 0;
 
-    return Obx(
-      () => BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Theme.of(context).primaryColor,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: 'Map',
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Theme.of(context).primaryColor,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: 'Map',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.arrow_upward,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.arrow_upward,
-            ),
-            label: 'List',
-          ),
-          existsPreviousProject
-              ? BottomNavigationBarItem(
-                  icon: Icon(Icons.arrow_back),
-                  label: 'Previous',
-                )
-              : BottomNavigationBarItem(
-                  icon: Icon(Icons.add),
-                  label: 'New',
-                ),
-          existsNextProject
-              ? BottomNavigationBarItem(
-                  icon: Icon(Icons.arrow_forward),
-                  label: 'Next',
-                )
-              : BottomNavigationBarItem(
-                  icon: Icon(Icons.add),
-                  label: 'New',
-                ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.arrow_downward),
-            label: 'Tables',
-          ),
-        ],
-        currentIndex: bottomBarIndex.value,
-        onTap: (index) async {
-          bottomBarIndex.value = index;
-          switch (index) {
-            case 0:
-              print('TODO:');
-              break;
-            case 1:
-              url.value = ['/projects/'];
-              break;
-            case 2:
-              {
-                if (!existsPreviousProject) {
-                  Project newProject = Project();
-                  await newProject.save();
-                  url.value = ['/projects/', newProject.id];
-                  break;
-                }
-                controller.previousPage(
-                    duration: Duration(milliseconds: 500), curve: Curves.ease);
-                break;
-              }
-            case 3:
-              {
-                if (!existsNextProject) {
-                  Project newProject = Project();
-                  await newProject.save();
-                  url.value = ['/projects/', newProject.id];
-                  break;
-                }
-                await controller.nextPage(
-                    duration: Duration(milliseconds: 500), curve: Curves.ease);
-                break;
-              }
-            case 4:
-              url.value = ['/projects/', project.id, '/children/'];
-              break;
-          }
-        },
-      ),
+          label: 'List',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.arrow_downward),
+          label: 'Tables',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add),
+          label: 'New',
+        ),
+      ],
+      currentIndex: 0,
+      onTap: (index) async {
+        switch (index) {
+          case 0:
+            print('TODO:');
+            break;
+          case 1:
+            url.value = ['/projects/'];
+            break;
+          case 2:
+            url.value = ['/projects/', project.id, '/children/'];
+            break;
+          case 3:
+            Project newProject = Project();
+            await newProject.save();
+            url.value = ['/projects/', newProject.id];
+            break;
+        }
+      },
     );
   }
 }
