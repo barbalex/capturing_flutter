3270a9db13852002bc14bd56a90dc77d5d0da9c9
Alexander Gabriel <alex.barbalex@gmail.com>
2021-05-27 16:17:54 +0200

    refactor

---
+++ lib/screens/tables/editable/list.dart
@@ -0,0 +1,104 @@
+import 'package:capturing/store.dart';
+import 'package:flutter/material.dart';
+import 'package:capturing/screens/tables/editable/tile.dart';
+import 'package:isar/isar.dart';
+import 'package:capturing/isar.g.dart';
+import 'package:get/get.dart';
+import 'dart:async';
+import 'package:capturing/models/table.dart';
+
+class TableList extends StatefulWidget {
+  @override
+  _TableListState createState() => _TableListState();
+}
+
+class _TableListState extends State<TableList> {
+  final Isar isar = Get.find<Isar>();
+  late StreamSubscription<void> tableListener;
+  final String projectId = activeProjectId ?? '';
+
+  @override
+  void initState() {
+    super.initState();
+    tableListener = isar.ctables
+        .where()
+        .filter()
+        .projectIdEqualTo(projectId)
+        .and()
+        .parentIdIsNull()
+        .and()
+        .deletedEqualTo(false)
+        .and()
+        // show tables with parent id only when editing structure
+        .optional(editingProject.value != projectId, (q) => q.parentIdIsNull())
+        .and()
+        // show option tables only when editing structure
+        .optional(
+            editingProject.value != projectId, (q) => q.optionTypeEqualTo(null))
+        .watchLazy()
+        .listen((_) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    tableListener.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //print('Table List, build, parentTableId: $parentTableId');
+    return FutureBuilder(
+      future: isar.ctables
+          .where()
+          .filter()
+          .projectIdEqualTo(projectId)
+          .and()
+          .deletedEqualTo(false)
+          .and()
+          // show tables with parent id only when editing structure
+          .optional(
+              editingProject.value != projectId, (q) => q.parentIdIsNull())
+          .and()
+          // show option tables only when editing structure
+          .optional(
+              editingProject.value != projectId, (q) => q.optionTypeIsNull())
+          .sortByName()
+          .findAll(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            Get.snackbar(
+              'Error accessing local storage',
+              snapshot.error.toString(),
+              snackPosition: SnackPosition.BOTTOM,
+            );
+          } else {
+            List<Ctable> tables = snapshot.data;
+            //print('TablesList, tables: $tables, projectId: $projectId');
+
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    TableTile(
+                      table: tables[index],
+                    ),
+                    Divider(
+                      height: 0,
+                      color: Theme.of(context).primaryColor.withOpacity(0.4),
+                    ),
+                  ],
+                );
+              },
+              itemCount: tables.length,
+            );
+          }
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
---
+++ lib/screens/tables/noneditable/index.dart
@@ -0,0 +1,147 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:capturing/models/table.dart';
+import 'package:capturing/models/project.dart';
+import 'package:capturing/screens/tables/noneditable/list.dart';
+import 'package:capturing/store.dart';
+import 'package:capturing/components/formTitle.dart';
+import 'package:isar/isar.dart';
+import 'package:capturing/isar.g.dart';
+
+class TablesNoneditable extends StatefulWidget {
+  @override
+  _TablesNoneditableState createState() => _TablesNoneditableState();
+}
+
+class _TablesNoneditableState extends State<TablesNoneditable> {
+  final String projectId = activeProjectId ?? '';
+  final Isar isar = Get.find<Isar>();
+  StreamSubscription<String>? editingProjectListener;
+
+  @override
+  void dispose() {
+    super.dispose();
+    editingProjectListener?.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<BottomNavigationBarItem> bottomNavigationBarItems = [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.map),
+        label: 'Map',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.arrow_upward,
+        ),
+        label: 'Project list',
+      ),
+    ];
+    if (editingProject.value == projectId) {
+      bottomNavigationBarItems.add(
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.arrow_upward,
+          ),
+          label: 'Project',
+        ),
+      );
+    }
+    editingProjectListener = editingProject.listen((_) {
+      setState(() {});
+    });
+
+    return FutureBuilder(
+      future: isar.projects.where().filter().idEqualTo(projectId).findFirst(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            Get.snackbar(
+              'Error accessing local storage',
+              snapshot.error.toString(),
+              snackPosition: SnackPosition.BOTTOM,
+            );
+          } else {
+            Project project = snapshot.data;
+            print('Tables, project: ${project.getLabel()}');
+
+            return Scaffold(
+              appBar: AppBar(
+                actions: <Widget>[
+                  Obx(
+                    () => IconButton(
+                      icon: Icon(
+                        Icons.build,
+                      ),
+                      onPressed: () {
+                        editingProject.value =
+                            editingProject.value == projectId ? '' : projectId;
+                      },
+                      tooltip: editingProject.value == projectId
+                          ? 'Editing data structure. Click to stop.'
+                          : 'Edit data structure',
+                      color: editingProject.value == projectId
+                          ? Theme.of(context).accentColor
+                          : Colors.white,
+                    ),
+                  ),
+                ],
+                title: FormTitle(title: 'Tables of ${project.getLabel()}'),
+              ),
+              body: TableList(),
+              bottomNavigationBar: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                backgroundColor: Theme.of(context).primaryColor,
+                selectedItemColor: Colors.white,
+                unselectedItemColor: Colors.white,
+                items: bottomNavigationBarItems,
+                currentIndex: 0,
+                onTap: (index) async {
+                  switch (index) {
+                    case 0:
+                      print('TODO: show map with all tables of this project');
+                      break;
+                    case 1:
+                      url.value = ['/projects/'];
+                      break;
+                    case 2:
+                      url.value = ['/projects/', projectId];
+                      break;
+                    case 3:
+                      // TODO: set url of parent
+                      //Get.toNamed();
+                      break;
+                  }
+                },
+              ),
+              // only show action button if user is account_admin
+              floatingActionButton: editingProject.value == projectId
+                  ? FloatingActionButton(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      child: Icon(
+                        Icons.add,
+                        size: 40,
+                      ),
+                      onPressed: () async {
+                        Ctable newTable = Ctable(projectId: projectId);
+                        await newTable.save();
+                        url.value = [
+                          '/projects/',
+                          projectId,
+                          '/children/',
+                          newTable.id
+                        ];
+                      },
+                      tooltip: 'Add Table',
+                    )
+                  : null,
+            );
+          }
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
