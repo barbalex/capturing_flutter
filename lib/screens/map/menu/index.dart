f12b74f2778ceaae9a679180d7c1397c7933dbf1
Alexander Gabriel <alex.barbalex@gmail.com>
2021-06-02 22:43:09 +0200

    work on map

--- lib/screens/map/menu/index.dart
+++ lib/screens/map/menu/index.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:meta/meta.dart';
 import 'determinePosition.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -40,58 +39,63 @@ class MapMenu extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.only(top: 50, left: 10),
-      child: Obx(
-        () => ToggleButtons(
-          children: [
-            Icon(Icons.my_location),
-            Icon(Icons.center_focus_weak_outlined),
-            Icon(Icons.linear_scale),
-            Icon(FontAwesomeIcons.drawPolygon),
-          ],
-          isSelected: toggleButtonsSelected,
-          onPressed: (int index) async {
-            toggleButtonsSelected[index] = true;
-            print('child $index was pressed');
-            switch (index) {
-              case 0:
-                Timer(
-                  Duration(seconds: 1),
-                  () {
-                    toggleButtonsSelected[index] = false;
-                  },
-                );
-                Position? position;
-                try {
-                  position = await determinePosition();
-                } catch (e) {
-                  Get.snackbar(
-                    'Error accessing position',
-                    e.toString(),
-                    snackPosition: SnackPosition.BOTTOM,
+      child: Container(
+        color: Theme.of(context).primaryColor.withOpacity(0.2),
+        child: Obx(
+          () => ToggleButtons(
+            children: [
+              Icon(Icons.my_location),
+              Icon(Icons.center_focus_weak_outlined),
+              Icon(Icons.linear_scale),
+              Icon(FontAwesomeIcons.drawPolygon),
+            ],
+            isSelected: toggleButtonsSelected,
+            onPressed: (int index) async {
+              toggleButtonsSelected[index] = true;
+              print('child $index was pressed');
+              switch (index) {
+                case 0:
+                  Timer(
+                    Duration(seconds: 1),
+                    () {
+                      toggleButtonsSelected[index] = false;
+                    },
                   );
-                }
-                if (position?.latitude != null && position?.longitude != null) {
-                  lat.value = position?.latitude ?? 0;
-                  lng.value = position?.longitude ?? 0;
-                  mapController.value.move(LatLng(lat.value, lng.value), 13);
-                }
-                break;
-              case 1:
-                toggleEditingPoints();
-                break;
-              case 2:
-                toggleEditingLines();
-                break;
-              case 3:
-                toggleEditingPolygons();
-                break;
-              default:
-            }
-          },
-          direction: Axis.vertical,
-          selectedColor: Theme.of(context).primaryColor,
-          fillColor: Theme.of(context).primaryColor.withOpacity(0.3),
-          borderColor: Theme.of(context).primaryColor,
+                  Position? position;
+                  try {
+                    position = await determinePosition();
+                  } catch (e) {
+                    Get.snackbar(
+                      'Error accessing position',
+                      e.toString(),
+                      snackPosition: SnackPosition.BOTTOM,
+                    );
+                  }
+                  if (position?.latitude != null &&
+                      position?.longitude != null) {
+                    lat.value = position?.latitude ?? 0;
+                    lng.value = position?.longitude ?? 0;
+                    mapController.value.move(LatLng(lat.value, lng.value), 13);
+                  }
+                  break;
+                case 1:
+                  toggleEditingPoints();
+                  break;
+                case 2:
+                  toggleEditingLines();
+                  break;
+                case 3:
+                  toggleEditingPolygons();
+                  break;
+                default:
+              }
+            },
+            direction: Axis.vertical,
+            selectedColor: Colors.white,
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.7),
+            selectedBorderColor: Theme.of(context).primaryColor,
+            borderColor: Theme.of(context).primaryColor,
+          ),
         ),
       ),
     );
