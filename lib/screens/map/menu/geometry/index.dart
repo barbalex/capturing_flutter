517cfc6d4311fc0b23cb8bc8b8452c99a7699a0a
Alexander Gabriel <alex.barbalex@gmail.com>
2021-06-04 15:09:49 +0200

    save linestring

--- lib/screens/map/menu/geometry/index.dart
+++ lib/screens/map/menu/geometry/index.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:get/get.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:capturing/store.dart';
+import 'package:geojson_vi/geojson_vi.dart';
+import 'package:isar/isar.dart';
+import 'package:capturing/isar.g.dart';
+import 'package:capturing/models/row.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapMenuGeometry extends StatelessWidget {
   final String mapGeometryType;
   final Function setMapGeometryType;
+  final List<LatLng> editingPolylinePoints;
+  final Function resetEditingPolylinePoints;
 
   MapMenuGeometry({
     required this.mapGeometryType,
     required this.setMapGeometryType,
+    required this.editingPolylinePoints,
+    required this.resetEditingPolylinePoints,
   });
 
   @override
   Widget build(BuildContext context) {
+    final Isar isar = Get.find<Isar>();
     final toggleButtonsSelected = <bool>[
       ['point', 'all'].contains(mapGeometryType),
       ['polyline', 'all'].contains(mapGeometryType),
@@ -56,13 +68,36 @@ class MapMenuGeometry extends StatelessWidget {
             case 1:
               if (mapGeometryType == 'polyline') {
                 setMapGeometryType('none');
+                // if editingPolylinePoints.length > 1
+                // save it to row.geometry
+                // and reset editingPolylinePoints
+                if (editingPolylinePoints.length < 2) return;
+                GeoJSONGeometryCollection geomCollection =
+                    activeRow?.geometry != null
+                        ? GeoJSONGeometryCollection.fromJSON(
+                            activeRow?.geometry ?? '')
+                        : GeoJSONGeometryCollection([]);
+                List<dynamic> geometries = geomCollection.geometries;
+                // TODO:
+                geometries.add(Polyline(points: editingPolylinePoints));
+                List<double>? bbox = geomCollection.bbox;
+                // 2. load from row
+                Crow? row = isar.crows
+                    .where()
+                    .idEqualTo(activeRowId ?? '')
+                    .findFirstSync();
+                if (row != null) {
+                  row.geometry = geomCollection.toJSON();
+                  row.geometryW = bbox[0];
+                  row.geometryS = bbox[1];
+                  row.geometryE = bbox[2];
+                  row.geometryN = bbox[3];
+                  row.save();
+                }
+                resetEditingPolylinePoints();
               } else {
                 setMapGeometryType('polyline');
               }
-              // TODO:
-              // if editingPolylinePoints.length > 1
-              // save it to row.geometry
-              // and reset editingPolylinePoints
               break;
             case 2:
             default:
