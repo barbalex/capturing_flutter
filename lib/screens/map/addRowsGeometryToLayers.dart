869017aaa22807cdabcb8bab0841486b7a5846a4
Alexander Gabriel <alex.barbalex@gmail.com>
2021-06-07 13:46:41 +0200

    refactor

---
+++ lib/screens/map/addRowsGeometryToLayers.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:capturing/models/row.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import './marker.dart';
+import './polyline.dart';
+import 'polygon.dart';
+import 'package:geojson_vi/geojson_vi.dart';
+import 'package:geodesy/geodesy.dart';
+
+void addRowsGeometryToLayers({
+  required BuildContext context,
+  required Crow row,
+  required RxList<Marker> markers,
+  required RxList<Polyline> polylines,
+  required RxList<Polygon> polygons,
+  required Function onTapMarker,
+}) {
+  // draw the geometry of this row
+  GeoJSONGeometryCollection? geomCollection =
+      GeoJSONGeometryCollection.fromJSON(row.geometry ?? '');
+  List<GeoJSONGeometry> geometries = geomCollection.geometries;
+  geometries.forEach((geometry) {
+    switch (geometry.type) {
+      case GeoJSONType.point:
+        GeoJSONPoint point = geometry as GeoJSONPoint;
+        markers.add(
+          // see: https://github.com/fleaflet/flutter_map/issues/184#issuecomment-446754375
+          MapMarker(
+            lng: point.coordinates[0],
+            lat: point.coordinates[1],
+            onTap: onTapMarker,
+          ),
+        );
+        break;
+      case GeoJSONType.lineString:
+        GeoJSONLineString polyline = geometry as GeoJSONLineString;
+        polylines.add(
+          MapPolyline(
+            points:
+                polyline.coordinates.map((e) => LatLng(e[1], e[0])).toList(),
+            context: context,
+          ),
+        );
+        break;
+      case GeoJSONType.polygon:
+        GeoJSONPolygon polygon = geometry as GeoJSONPolygon;
+        polygons.add(
+          MapPolygon(
+            points: polygon.coordinates
+                .expand(
+                  (a) => a.map((e) => LatLng(e[1], e[0])).toList(),
+                )
+                .toList(),
+            context: context,
+          ),
+        );
+        break;
+      default:
+        print('don\'t know this geometry\'s type');
+    }
+  });
+}
--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -12,67 +12,12 @@ import './scale_layer_plugin_option.dart';
 import './marker.dart';
 import './polylineMarker.dart';
 import './editingPolyline.dart';
-import './polyline.dart';
-import 'polygon.dart';
+import './addRowsGeometryToLayers.dart';
 import 'package:geojson_vi/geojson_vi.dart';
 import 'package:capturing/store.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'menu/index.dart';
 import 'package:geodesy/geodesy.dart';
-
-void addRowsGeometryToLayers({
-  required BuildContext context,
-  required Crow row,
-  required RxList<Marker> markers,
-  required RxList<Polyline> polylines,
-  required RxList<Polygon> polygons,
-  required Function onTapMarker,
-}) {
-  // draw the geometry of this row
-  GeoJSONGeometryCollection? geomCollection =
-      GeoJSONGeometryCollection.fromJSON(row.geometry ?? '');
-  List<GeoJSONGeometry> geometries = geomCollection.geometries;
-  geometries.forEach((geometry) {
-    switch (geometry.type) {
-      case GeoJSONType.point:
-        GeoJSONPoint point = geometry as GeoJSONPoint;
-        markers.add(
-          // see: https://github.com/fleaflet/flutter_map/issues/184#issuecomment-446754375
-          MapMarker(
-            lng: point.coordinates[0],
-            lat: point.coordinates[1],
-            onTap: onTapMarker,
-          ),
-        );
-        break;
-      case GeoJSONType.lineString:
-        GeoJSONLineString polyline = geometry as GeoJSONLineString;
-        polylines.add(
-          MapPolyline(
-            points:
-                polyline.coordinates.map((e) => LatLng(e[1], e[0])).toList(),
-            context: context,
-          ),
-        );
-        break;
-      case GeoJSONType.polygon:
-        GeoJSONPolygon polygon = geometry as GeoJSONPolygon;
-        polygons.add(
-          MapPolygon(
-            points: polygon.coordinates
-                .expand(
-                  (a) => a.map((e) => LatLng(e[1], e[0])).toList(),
-                )
-                .toList(),
-            context: context,
-          ),
-        );
-        break;
-      default:
-        print('don\'t know this geometry\'s type');
-    }
-  });
-}
 
 class MapMapWidget extends StatefulWidget {
   @override
