0641e96e9c813f9275e930a1cb45f5e3ec9e02ed
Alexander Gabriel <alex.barbalex@gmail.com>
2021-06-07 14:49:28 +0200

    refactor calculation of active ids

--- lib/store.dart
+++ lib/store.dart
@@ -31,28 +31,18 @@ mayEdit(String projectId) =>
 String? get activeRowId {
   // find last rows folder containing a row id
   if (!url.contains('/rows/')) return null;
-  List<String> urlCopied = [...url];
-  String? lastRowFolder =
-      urlCopied.lastWhere((e) => e == '/rows/', orElse: null);
-  // ignore: unnecessary_null_comparison
-  if (lastRowFolder == null) return null;
-  int indexOfLastRowFolder = urlCopied.indexOf(lastRowFolder);
-  if (urlCopied.length > indexOfLastRowFolder + 1) {
-    String lastRowFoldersChild = urlCopied[indexOfLastRowFolder + 1];
-    if (validator.isUUID(lastRowFoldersChild)) return lastRowFoldersChild;
-  }
-  // shorten the url
-  urlCopied.length = indexOfLastRowFolder;
-  // look if there is a parent row
-  lastRowFolder = urlCopied.lastWhere((e) => e == '/rows/', orElse: null);
-  // ignore: unnecessary_null_comparison
-  if (lastRowFolder == null) return null;
-  indexOfLastRowFolder = urlCopied.indexOf(lastRowFolder);
-  if (urlCopied.length > indexOfLastRowFolder + 1) {
-    String lastRowFoldersChild = urlCopied[indexOfLastRowFolder + 1];
-    if (validator.isUUID(lastRowFoldersChild)) return lastRowFoldersChild;
-  }
-  return null;
+  String lastRowFolder = url.lastWhere((e) {
+    if (e != '/rows/') return false;
+    int childIndex = url.indexOf(e) + 1;
+    bool hasChild = url.length > childIndex;
+    if (!hasChild) return false;
+    String child = url[childIndex];
+    bool childIsUuid = validator.isUUID(child);
+    return childIsUuid;
+  }, orElse: () => '');
+  if (lastRowFolder == '') return null;
+  String activeRowId = url[url.indexOf(lastRowFolder) + 1];
+  return activeRowId;
 }
 
 Crow? get activeRow {
@@ -65,27 +55,18 @@ String? get activeTableId {
   // and return the uuid
   // else: return null
   if (!url.contains('/tables/')) return null;
-  List<String> urlCopied = [...url];
-  String? lastTableFolder =
-      urlCopied.lastWhere((e) => e == '/tables/', orElse: null);
-  // ignore: unnecessary_null_comparison
-  if (lastTableFolder == null) return null;
-  int indexOfLastTableFolder = urlCopied.indexOf(lastTableFolder);
-  if (urlCopied.length > indexOfLastTableFolder + 1) {
-    String lastTableFoldersChild = urlCopied[indexOfLastTableFolder + 1];
-    if (validator.isUUID(lastTableFoldersChild)) return lastTableFoldersChild;
-  }
-  // shorten the url
-  urlCopied.length = indexOfLastTableFolder;
-  lastTableFolder = urlCopied.lastWhere((e) => e == '/tables/', orElse: null);
-  // ignore: unnecessary_null_comparison
-  if (lastTableFolder == null) return null;
-  indexOfLastTableFolder = urlCopied.indexOf(lastTableFolder);
-  if (urlCopied.length > indexOfLastTableFolder + 1) {
-    String lastTableFoldersChild = urlCopied[indexOfLastTableFolder + 1];
-    if (validator.isUUID(lastTableFoldersChild)) return lastTableFoldersChild;
-  }
-  return null;
+  String lastTableFolder = url.lastWhere((e) {
+    if (e != '/tables/') return false;
+    int childIndex = url.indexOf(e) + 1;
+    bool hasChild = url.length > childIndex;
+    if (!hasChild) return false;
+    String child = url[childIndex];
+    bool childIsUuid = validator.isUUID(child);
+    return childIsUuid;
+  }, orElse: () => '');
+  if (lastTableFolder == '') return null;
+  String activeTableId = url[url.indexOf(lastTableFolder) + 1];
+  return activeTableId;
 }
 
 Ctable? get activeTable {
