b9bba5f80aca81d6f29baff3fca16f2da888f545
Alexander Gabriel <alex.barbalex@gmail.com>
2021-06-14 16:53:50 +0200

    test server

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,15 +1,14 @@
-import 'package:flutter/foundation.dart';
+//import 'package:flutter/foundation.dart';
 
-bool get isDev => kReleaseMode ? false : true;
+//bool get isDev => kReleaseMode ? false : true;
+bool get isDev => true;
 
 // magic address for local dev, see: https://github.com/Flutterando/hasura_connect/issues/7#issuecomment-539441669
-String get graphQlUri => isDev
-    ? 'http://10.0.2.2:8080/v1/graphql'
-    : 'https://api.capturing.app/v1/graphql';
+String get graphQlUri =>
+    isDev ? 'http://10.0.2.2:8080/v1/graphql' : 'https://api.capturing.app';
 
-String get wsGraphQlUri => isDev
-    ? 'ws://10.0.2.2:8080/v1/graphql'
-    : 'wss://api.capturing.app/v1/graphql';
+String get wsGraphQlUri =>
+    isDev ? 'ws://10.0.2.2:8080/v1/graphql' : 'wss://api.capturing.app';
 
 String get graphQlHealthUri => isDev
     ? 'http://localhost:8080/healthz'
