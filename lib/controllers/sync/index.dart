5e39612b4cb89f4cda94a173b8c40dc7048a669c
Alexander Gabriel <alex.barbalex@gmail.com>
2021-06-18 17:37:03 +0200

    clean

--- lib/controllers/sync/index.dart
+++ lib/controllers/sync/index.dart
@@ -9,7 +9,7 @@ import 'package:isar/isar.dart';
 import 'package:capturing/isar.g.dart';
 import 'package:capturing/controllers/sync/dbOperations/index.dart';
 import 'package:capturing/controllers/sync/fileOperations/index.dart';
-import 'package:capturing/controllers/sync/subscribeFromServer.dart';
+//import 'package:capturing/controllers/sync/subscribeFromServer.dart';
 import 'package:capturing/controllers/sync/queryFromServer.dart';
 import 'package:capturing/controllers/sync/updateFromServer.dart';
 import 'package:capturing/controllers/sync/tokenInterceptor.dart';
