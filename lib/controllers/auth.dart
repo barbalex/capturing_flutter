ba7daf3e94296aaf3b14f945860da251540856d1
Alexander Gabriel <alex.barbalex@gmail.com>
2021-06-22 11:00:09 +0200

    clean

--- lib/controllers/auth.dart
+++ lib/controllers/auth.dart
@@ -72,7 +72,7 @@ class AuthController extends GetxController {
           snackPosition: SnackPosition.BOTTOM,
         );
       }
-      print('authController, token: ${token.value}');
+      //print('authController, token: ${token.value}');
       activeUserEmail.value = _firebaseUser?.value?.email ?? '';
       setActiveUserHasAccount();
       if (_firebaseUser?.value?.email != null) {
