eebaea41980cbc8576094877356d15aafcd1ee2f
Alexander Gabriel <alex.barbalex@gmail.com>
2021-06-22 13:04:46 +0200

    refactor

--- lib/screens/fields/index.dart
+++ lib/screens/fields/index.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:capturing/models/field.dart';
@@ -61,103 +59,80 @@ class _FieldsListContainerState extends State<FieldsListContainer> {
     bool mayEditStructure =
         ['project_manager', 'account_manager'].contains(activeUserRole);
 
-    return FutureBuilder(
-      future:
-          isar.ctables.where().filter().idEqualTo(tableId ?? '').findFirst(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            Get.snackbar(
-              'Error accessing local storage',
-              snapshot.error.toString(),
-              snackPosition: SnackPosition.BOTTOM,
-            );
-          } else {
-            Ctable? table = snapshot.data;
+    Ctable? table =
+        isar.ctables.where().filter().idEqualTo(tableId ?? '').findFirstSync();
 
-            return Scaffold(
-              appBar: AppBar(
-                actions: <Widget>[
-                  // only show actions if user is account_admin
-                  mayEditStructure
-                      ? Obx(
-                          () => IconButton(
-                            icon: Icon(
-                              Icons.build,
-                            ),
-                            onPressed: () {
-                              editingProject.value =
-                                  editingProject.value == projectId
-                                      ? ''
-                                      : projectId;
-                            },
-                            tooltip: editingProject.value == projectId
-                                ? 'Editing data structure. Click to stop.'
-                                : 'Edit data structure',
-                            color: editingProject.value == projectId
-                                ? Theme.of(context).accentColor
-                                : Colors.white,
-                          ),
-                        )
-                      : Container(),
-                ],
-                title: FormTitle(title: 'Fields of ${table?.getLabel()}'),
-              ),
-              body: FieldList(),
-              bottomNavigationBar: BottomNavigationBar(
-                type: BottomNavigationBarType.fixed,
-                backgroundColor: Theme.of(context).primaryColor,
-                selectedItemColor: Colors.white,
-                unselectedItemColor: Colors.white,
-                items: bottomNavigationBarItems,
-                currentIndex: 0,
-                onTap: (index) async {
-                  switch (index) {
-                    case 0:
-                      url.value = [...url, 'map/'];
-                      break;
-                    case 1:
-                      url.value = ['/projects/', projectId, '/tables/'];
-                      break;
-                    case 2:
-                      if (tableId == null) break;
-                      url.value = [
-                        '/projects/',
-                        projectId,
-                        '/tables/',
-                        tableId ?? ''
-                      ];
-                      break;
-                  }
-                },
-              ),
-              // TODO: only show action button if user is account_admin
-              floatingActionButton: FloatingActionButton(
-                backgroundColor: Theme.of(context).primaryColor,
-                child: Icon(
-                  Icons.add,
-                  size: 40,
-                ),
-                onPressed: () async {
-                  if (tableId == null) return;
-                  Field newField = Field(tableId: tableId);
-                  await newField.create();
-                  url.value = [
-                    '/projects/',
-                    projectId,
-                    '/tables/',
-                    tableId ?? '',
-                    '/fields/',
-                    newField.id
-                  ];
-                },
-                tooltip: 'Add Field',
-              ),
-            );
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          // only show actions if user is account_admin
+          mayEditStructure
+              ? Obx(
+                  () => IconButton(
+                    icon: Icon(
+                      Icons.build,
+                    ),
+                    onPressed: () {
+                      editingProject.value =
+                          editingProject.value == projectId ? '' : projectId;
+                    },
+                    tooltip: editingProject.value == projectId
+                        ? 'Editing data structure. Click to stop.'
+                        : 'Edit data structure',
+                    color: editingProject.value == projectId
+                        ? Theme.of(context).accentColor
+                        : Colors.white,
+                  ),
+                )
+              : Container(),
+        ],
+        title: FormTitle(title: 'Fields of ${table?.getLabel()}'),
+      ),
+      body: FieldList(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        items: bottomNavigationBarItems,
+        currentIndex: 0,
+        onTap: (index) async {
+          switch (index) {
+            case 0:
+              url.value = [...url, 'map/'];
+              break;
+            case 1:
+              url.value = ['/projects/', projectId, '/tables/'];
+              break;
+            case 2:
+              if (tableId == null) break;
+              url.value = ['/projects/', projectId, '/tables/', tableId ?? ''];
+              break;
           }
-        }
-        return CircularProgressIndicator();
-      },
+        },
+      ),
+      // TODO: only show action button if user is account_admin
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(
+          Icons.add,
+          size: 40,
+        ),
+        onPressed: () async {
+          if (tableId == null) return;
+          Field newField = Field(tableId: tableId);
+          await newField.create();
+          url.value = [
+            '/projects/',
+            projectId,
+            '/tables/',
+            tableId ?? '',
+            '/fields/',
+            newField.id
+          ];
+        },
+        tooltip: 'Add Field',
+      ),
     );
   }
 }
