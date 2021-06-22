68ad350d2396dec7a55908c0f22202f116934956
Alexander Gabriel <alex.barbalex@gmail.com>
2021-06-22 13:23:57 +0200

    remove FutureBuilder

--- lib/screens/row/radioGroup.dart
+++ lib/screens/row/radioGroup.dart
@@ -35,62 +35,44 @@ class _RadioGroupWidgetState extends State<RadioGroupWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: isar.crows
-          .where()
-          .filter()
-          .tableIdEqualTo(widget.field.optionsTable ?? '')
-          .findAll(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            Get.snackbar(
-              'Error accessing local storage',
-              snapshot.error.toString(),
-              snackPosition: SnackPosition.BOTTOM,
-            );
-          } else {
-            if (snapshot.data == null) return Container();
-            List<Crow> options = snapshot.data;
-            Map<String, dynamic> data = widget.row.getData();
-            //print('DropdownWidget, options: $options, data: $data');
+    List<Crow> options = isar.crows
+        .where()
+        .filter()
+        .tableIdEqualTo(widget.field.optionsTable ?? '')
+        .findAllSync();
+    Map<String, dynamic> data = widget.row.getData();
 
-            return FormBuilderRadioGroup<String?>(
-              name: widget.field.id,
-              validator: (_) {
-                if (errorText.value != '') return errorText.value;
-                return null;
-              },
-              onChanged: (choosen) async {
-                if (choosen == data['${widget.field.name}']) choosen = null;
-                print('choosen: $choosen');
-                data['${widget.field.name}'] = choosen;
-                widget.row.data = json.encode(data);
-                try {
-                  await widget.row.saveData(
-                      fieldName: widget.field.name ?? '', value: choosen);
-                  errorText.value = '';
-                } catch (e) {
-                  print(e);
-                  errorText.value = e.toString();
-                }
-              },
-              decoration: InputDecoration(
-                labelText: widget.field.label ?? widget.field.name,
-              ),
-              initialValue: data['${widget.field.name}'] != null
-                  ? data['${widget.field.name}']
-                  : null,
-              options: options.map((option) {
-                Map data = option.getData();
-                return FormBuilderFieldOption(value: data['value'] as String);
-              }).toList(growable: false),
-              orientation: OptionsOrientation.vertical,
-            );
-          }
-        }
-        return CircularProgressIndicator();
+    return FormBuilderRadioGroup<String?>(
+      name: widget.field.id,
+      validator: (_) {
+        if (errorText.value != '') return errorText.value;
+        return null;
       },
+      onChanged: (choosen) async {
+        if (choosen == data['${widget.field.name}']) choosen = null;
+        print('choosen: $choosen');
+        data['${widget.field.name}'] = choosen;
+        widget.row.data = json.encode(data);
+        try {
+          await widget.row
+              .saveData(fieldName: widget.field.name ?? '', value: choosen);
+          errorText.value = '';
+        } catch (e) {
+          print(e);
+          errorText.value = e.toString();
+        }
+      },
+      decoration: InputDecoration(
+        labelText: widget.field.label ?? widget.field.name,
+      ),
+      initialValue: data['${widget.field.name}'] != null
+          ? data['${widget.field.name}']
+          : null,
+      options: options.map((option) {
+        Map data = option.getData();
+        return FormBuilderFieldOption(value: data['value'] as String);
+      }).toList(growable: false),
+      orientation: OptionsOrientation.vertical,
     );
   }
 }
