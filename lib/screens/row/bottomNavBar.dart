a142c95c9be4129da1554db1509d76ce681c0c11
Alexander Gabriel <alex.barbalex@gmail.com>
2021-06-22 13:23:49 +0200

    remove FutureBuilder

--- lib/screens/row/bottomNavBar.dart
+++ lib/screens/row/bottomNavBar.dart
@@ -16,117 +16,93 @@ class BottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Future.wait([
-        isar.ctables
-            .where()
-            .filter()
-            .parentIdEqualTo(row?.tableId ?? 'none')
-            .and()
-            .deletedEqualTo(false)
-            .sortByOrd()
-            .findAll(),
-      ]),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            Get.snackbar(
-              'Error accessing local storage',
-              snapshot.error.toString(),
-              snackPosition: SnackPosition.BOTTOM,
-            );
-          } else {
-            List<Ctable> childTables = snapshot.data?[0] ?? [];
-            int childTableCount = childTables.length;
-            int newNavbarItemIndex = 2 + childTableCount;
+    List<Ctable> childTables = isar.ctables
+        .where()
+        .filter()
+        .parentIdEqualTo(row?.tableId ?? 'none')
+        .and()
+        .deletedEqualTo(false)
+        .sortByOrd()
+        .findAllSync();
+    int childTableCount = childTables.length;
+    int newNavbarItemIndex = 2 + childTableCount;
 
-            return BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              backgroundColor: Theme.of(context).primaryColor,
-              selectedItemColor: Colors.white,
-              unselectedItemColor: Colors.white,
-              items: <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.map),
-                  label: 'Map',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.arrow_upward),
-                  label: 'List',
-                ),
-                ...childTables
-                    .map(
-                      (t) => BottomNavigationBarItem(
-                        icon: Icon(Icons.arrow_downward),
-                        label: t.getLabel(),
-                      ),
-                    )
-                    .toList(),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.add),
-                  label: 'New',
-                ),
-              ],
-              currentIndex: 0,
-              onTap: (index) async {
-                if (index == 0) {
-                  // need to find active row id from pagehistory and rows
-                  // and set it in url
-                  // so the map knows what row is active
-                  Crow activeRow = rows[pageHistory.last];
-                  List<String> newUrl = [...url];
-                  if (newUrl.length > 0) newUrl.removeLast();
-                  newUrl.add(activeRow.id);
-                  newUrl.add('/map/');
-                  url.value = newUrl;
-                } else if (index == 1) {
-                  List<String> newUrl = [...url];
-                  if (newUrl.length > 0) newUrl.removeLast();
-                  url.value = newUrl;
-                } else if (index == newNavbarItemIndex) {
-                  String? tableId = row?.tableId;
-                  // add row's parent row id if table has parentId
-                  List<String> urlCopied = [...url];
-                  // remove last rows folder and own id
-                  if (urlCopied.length > 1) {
-                    urlCopied.removeLast();
-                    urlCopied.removeLast();
-                  }
-                  int indexOfLastRowsFolder =
-                      urlCopied.lastIndexWhere((e) => e == '/rows/');
-                  String? parentRowId = indexOfLastRowsFolder == -1
-                      ? null
-                      : urlCopied.length > indexOfLastRowsFolder
-                          ? urlCopied[indexOfLastRowsFolder + 1]
-                          : null;
-                  Crow newRow = Crow(tableId: tableId, parentId: parentRowId);
-                  await newRow.create();
-                  List<String> newUrl = [...url];
-                  if (url.length > 0) {
-                    newUrl.removeLast();
-                    newUrl.add(newRow.id);
-                    url.value = newUrl;
-                  }
-                } else {
-                  // this is a child table
-                  Ctable? childTable;
-                  childTable =
-                      childTables.length > 1 ? childTables[index - 2] : null;
-                  if (childTable != null) {
-                    List<String> newUrl = [
-                      ...url,
-                      '/tables/',
-                      childTable.id,
-                      '/rows/'
-                    ];
-                    url.value = newUrl;
-                  }
-                }
-              },
-            );
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Theme.of(context).primaryColor,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: 'Map',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.arrow_upward),
+          label: 'List',
+        ),
+        ...childTables
+            .map(
+              (t) => BottomNavigationBarItem(
+                icon: Icon(Icons.arrow_downward),
+                label: t.getLabel(),
+              ),
+            )
+            .toList(),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add),
+          label: 'New',
+        ),
+      ],
+      currentIndex: 0,
+      onTap: (index) async {
+        if (index == 0) {
+          // need to find active row id from pagehistory and rows
+          // and set it in url
+          // so the map knows what row is active
+          Crow activeRow = rows[pageHistory.last];
+          List<String> newUrl = [...url];
+          if (newUrl.length > 0) newUrl.removeLast();
+          newUrl.add(activeRow.id);
+          newUrl.add('/map/');
+          url.value = newUrl;
+        } else if (index == 1) {
+          List<String> newUrl = [...url];
+          if (newUrl.length > 0) newUrl.removeLast();
+          url.value = newUrl;
+        } else if (index == newNavbarItemIndex) {
+          String? tableId = row?.tableId;
+          // add row's parent row id if table has parentId
+          List<String> urlCopied = [...url];
+          // remove last rows folder and own id
+          if (urlCopied.length > 1) {
+            urlCopied.removeLast();
+            urlCopied.removeLast();
+          }
+          int indexOfLastRowsFolder =
+              urlCopied.lastIndexWhere((e) => e == '/rows/');
+          String? parentRowId = indexOfLastRowsFolder == -1
+              ? null
+              : urlCopied.length > indexOfLastRowsFolder
+                  ? urlCopied[indexOfLastRowsFolder + 1]
+                  : null;
+          Crow newRow = Crow(tableId: tableId, parentId: parentRowId);
+          await newRow.create();
+          List<String> newUrl = [...url];
+          if (url.length > 0) {
+            newUrl.removeLast();
+            newUrl.add(newRow.id);
+            url.value = newUrl;
+          }
+        } else {
+          // this is a child table
+          Ctable? childTable;
+          childTable = childTables.length > 1 ? childTables[index - 2] : null;
+          if (childTable != null) {
+            List<String> newUrl = [...url, '/tables/', childTable.id, '/rows/'];
+            url.value = newUrl;
           }
         }
-        return CircularProgressIndicator();
       },
     );
   }
