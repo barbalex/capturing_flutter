9d99f1d21937a9b53b55b08cb01adaa8ad7a25f7
Alexander Gabriel <alex.barbalex@gmail.com>
2021-06-22 13:23:31 +0200

    remove FutureBuilder

--- lib/screens/project/index.dart
+++ lib/screens/project/index.dart
@@ -18,63 +18,43 @@ class ProjectContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //print('project, id: $id');
-    return FutureBuilder(
-      //future: isar.projects.where().idEqualTo(id).findFirst(),
-      future: isar.projects
-          .where()
-          .filter()
-          .deletedEqualTo(false)
-          .sortByName()
-          .findAll(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            Get.snackbar(
-              'Error accessing local storage',
-              snapshot.error.toString(),
-              snackPosition: SnackPosition.BOTTOM,
-            );
-          } else {
-            if (snapshot.data == null) return Container();
-            List<Project> projects = snapshot.data;
-            Project? project = projects.firstWhereOrNull((p) => p.id == id);
-            activePageIndex.value =
-                project != null ? projects.indexOf(project) : 0;
-            final PageController controller =
-                PageController(initialPage: activePageIndex.value);
-            List<String> urlOnEntering = [...url];
+    List<Project> projects = isar.projects
+        .where()
+        .filter()
+        .deletedEqualTo(false)
+        .sortByName()
+        .findAllSync();
+    Project? project = projects.firstWhereOrNull((p) => p.id == id);
+    activePageIndex.value = project != null ? projects.indexOf(project) : 0;
+    final PageController controller =
+        PageController(initialPage: activePageIndex.value);
+    List<String> urlOnEntering = [...url];
 
-            return WillPopScope(
-              // PageView does not navigate using navigator
-              // so when user pops, need to use self-built pageHistory
-              // and navigate back using that to enable expected experience
-              onWillPop: () {
-                if (pageHistory.length > 1) {
-                  pageHistory.removeLast();
-                  controller.animateToPage(
-                    pageHistory.last,
-                    duration: Duration(milliseconds: 500),
-                    curve: Curves.ease,
-                  );
-                  return Future.value(false);
-                }
-                print('urlOnEntering: urlOnEntering');
-                urlOnEntering.removeLast();
-                return Future.value(true);
-              },
-              child: Scaffold(
-                appBar: AppBar(
-                  title: FormTitle(title: 'Project'),
-                ),
-                body: ProjectWidget(),
-                bottomNavigationBar: BottomNavBar(projects: projects),
-              ),
-            );
-          }
+    return WillPopScope(
+      // PageView does not navigate using navigator
+      // so when user pops, need to use self-built pageHistory
+      // and navigate back using that to enable expected experience
+      onWillPop: () {
+        if (pageHistory.length > 1) {
+          pageHistory.removeLast();
+          controller.animateToPage(
+            pageHistory.last,
+            duration: Duration(milliseconds: 500),
+            curve: Curves.ease,
+          );
+          return Future.value(false);
         }
-        return CircularProgressIndicator();
+        print('urlOnEntering: urlOnEntering');
+        urlOnEntering.removeLast();
+        return Future.value(true);
       },
+      child: Scaffold(
+        appBar: AppBar(
+          title: FormTitle(title: 'Project'),
+        ),
+        body: ProjectWidget(),
+        bottomNavigationBar: BottomNavBar(projects: projects),
+      ),
     );
   }
 }
