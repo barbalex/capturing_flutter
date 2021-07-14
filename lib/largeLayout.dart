97ad59ba40e90d526077343fe570bbe1a22e5035
Alexander Gabriel <alex.barbalex@gmail.com>
2021-07-14 16:33:51 +0200

    work on large layout

--- lib/largeLayout.dart
+++ lib/largeLayout.dart
@@ -9,6 +9,7 @@ import 'package:capturing/screens/tree/index.dart';
 import 'package:easy_debounce/easy_debounce.dart';
 import 'package:capturing/store.dart';
 import 'package:capturing/models/store.dart';
+import 'package:geolocator_platform_interface/geolocator_platform_interface.dart';
 import 'package:isar/isar.dart';
 import 'isar.g.dart';
 import 'package:get/get.dart';
@@ -31,44 +32,58 @@ class _LargeLayoutState extends State<LargeLayout> {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: LayoutGrid(
-        columnSizes: [largeLayoutTreeColumnSize.value.px, 10.px, 1.fr],
+        columnSizes: [largeLayoutTreeColumnSize.value.px, 12.px, 1.fr],
         rowSizes: [1.fr],
         children: [
           Tree(),
           MouseRegion(
             cursor: SystemMouseCursors.resizeLeftRight,
-            child: Draggable(
-              child: VerticalDivider(
-                color: Theme.of(context).primaryColor.withOpacity(0.1),
-                thickness: 10,
-                width: 10,
-              ),
-              feedback: Container(),
-              onDragUpdate: (details) {
-                EasyDebounce.debounce(
-                    'layoutDivider', Duration(milliseconds: 1), () {
-                  int newVal = details.globalPosition.dx.toInt();
-                  if (newVal != largeLayoutTreeColumnSize.value) {
-                    largeLayoutTreeColumnSize.value = newVal;
-                    setState(() {});
-                  }
-                });
+            child: Stack(
+              children: [
+                Center(
+                    child: Container(
+                  child: Icon(
+                    Icons.drag_indicator,
+                    color: Theme.of(context).primaryColor.withOpacity(0.3),
+                  ),
+                  transform: Matrix4.translationValues(-6, 0, 0),
+                )),
+                Expanded(
+                  child: Draggable(
+                    child: VerticalDivider(
+                      color: Theme.of(context).primaryColor.withOpacity(0.1),
+                      thickness: 12,
+                      width: 12,
+                    ),
+                    feedback: Container(),
+                    onDragUpdate: (details) {
+                      EasyDebounce.debounce(
+                          'layoutDivider', Duration(milliseconds: 1), () {
+                        int newVal = details.globalPosition.dx.toInt();
+                        if (newVal != largeLayoutTreeColumnSize.value) {
+                          largeLayoutTreeColumnSize.value = newVal;
+                          setState(() {});
+                        }
+                      });
 
-                final Isar isar = Get.find<Isar>();
-                EasyDebounce.debounce(
-                    'treeColumnSizeSetting', Duration(milliseconds: 200),
-                    () async {
-                  Store? store = await isar.stores.get(1);
-                  if (store?.largeLayoutTreeColumnSize !=
-                      largeLayoutTreeColumnSize.value) {
-                    await isar.writeTxn((_) async {
-                      store?.largeLayoutTreeColumnSize =
-                          largeLayoutTreeColumnSize.value;
-                      await isar.stores.put(store as Store);
-                    });
-                  }
-                });
-              },
+                      final Isar isar = Get.find<Isar>();
+                      EasyDebounce.debounce(
+                          'treeColumnSizeSetting', Duration(milliseconds: 200),
+                          () async {
+                        Store? store = await isar.stores.get(1);
+                        if (store?.largeLayoutTreeColumnSize !=
+                            largeLayoutTreeColumnSize.value) {
+                          await isar.writeTxn((_) async {
+                            store?.largeLayoutTreeColumnSize =
+                                largeLayoutTreeColumnSize.value;
+                            await isar.stores.put(store as Store);
+                          });
+                        }
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
           widget.content,
