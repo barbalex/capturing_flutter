6b3201c115cb4ef7dc15e5bdb1a948d9bbf3c09d
Alexander Gabriel <alex.barbalex@gmail.com>
2021-07-15 10:05:36 +0200

    refactor

--- lib/screens/tables/noneditable/index.dart
+++ lib/screens/tables/noneditable/index.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:capturing/models/project.dart';
 import 'package:capturing/screens/tables/noneditable/list.dart';
+import 'package:capturing/screens/tables/noneditable/bottomNavBar.dart';
 import 'package:capturing/store.dart';
 import 'package:capturing/components/formTitle.dart';
 import 'package:isar/isar.dart';
@@ -37,40 +38,7 @@ class TablesNoneditable extends StatelessWidget {
         semanticLabel: 'Tree view of the data structure',
       ),
       body: TablesNoneditableList(),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Theme.of(context).primaryColor,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: 'Map'.tr,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.arrow_upward),
-            label: 'Project List'.tr,
-          ),
-        ],
-        currentIndex: 0,
-        onTap: (index) async {
-          switch (index) {
-            case 0:
-              url.value = [...url, 'map/'];
-              break;
-            case 1:
-              url.value = ['/projects/'];
-              break;
-            case 2:
-              url.value = ['/projects/', projectId];
-              break;
-            case 3:
-              // TODO: set url of parent
-              //Get.toNamed();
-              break;
-          }
-        },
-      ),
+      bottomNavigationBar: TablesNoneditableBottomNavBar(),
     );
   }
 }
