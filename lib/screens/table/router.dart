db919ac922d1245ceb1eca73785acded12dbddfd
Alexander Gabriel <alex.barbalex@gmail.com>
2021-07-15 11:05:49 +0200

    prepare table router

---
+++ lib/screens/table/router.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:capturing/utils/getLayout.dart';
+import 'package:capturing/screens/table/index.dart';
+import 'package:capturing/screens/table/bottomNavBar.dart';
+import 'package:capturing/largeLayout.dart';
+import 'package:get/get.dart';
+import 'package:capturing/screens/table/table.dart';
+import 'package:isar/isar.dart';
+import 'package:capturing/isar.g.dart';
+import 'package:capturing/store.dart';
+import 'package:capturing/models/project.dart';
+import 'package:capturing/models/table.dart';
+import 'package:collection/collection.dart';
+
+class ProjectsRouter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String layout = getLayout(context: context);
+
+    if (layout == 'sm') return TableContainer();
+    final Isar isar = Get.find<Isar>();
+
+    Project? project = isar.projects
+        .where()
+        .filter()
+        .idEqualTo(activeProjectId ?? '')
+        .findFirstSync();
+    List<Ctable> tables = isar.ctables
+        .where()
+        .filter()
+        .deletedEqualTo(false)
+        .and()
+        .projectIdEqualTo(activeProjectId)
+        .sortByOrd()
+        .findAllSync();
+    Ctable? table = tables.where((p) => p.id == activeTableId).firstOrNull;
+    if (table == null) return Container();
+
+    return LargeLayout(
+      content: TableWidget(tables: tables, table: table),
+      title: '${'Table of'.tr} ${project?.getLabel()}',
+      bottomNavBar: TableBottomNavBar(),
+    );
+  }
+}
