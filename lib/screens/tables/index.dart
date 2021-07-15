796a89ffb1568df795f5bd22765ec06a47dc3540
Alexander Gabriel <alex.barbalex@gmail.com>
2021-07-15 09:59:49 +0200

    lg layout for tables noneditable

--- lib/screens/tables/index.dart
+++ lib/screens/tables/index.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:capturing/store.dart';
 import 'package:isar/isar.dart';
+import 'package:capturing/isar.g.dart';
 import 'package:capturing/screens/tables/editable/index.dart';
+import 'package:capturing/screens/tables/editable/bottomNavBar.dart';
 import 'package:capturing/screens/tables/noneditable/index.dart';
+import 'package:capturing/screens/tables/noneditable/list.dart';
+import 'package:capturing/utils/getLayout.dart';
+import 'package:capturing/largeLayout.dart';
+import 'package:capturing/models/project.dart';
 
 class TablesListRouter extends StatefulWidget {
   @override
@@ -24,9 +30,19 @@ class _TablesListRouterState extends State<TablesListRouter> {
 
   @override
   Widget build(BuildContext context) {
+    String layout = getLayout(context: context);
+    Project? project =
+        isar.projects.where().filter().idEqualTo(projectId).findFirstSync();
+
     if (editingProject.value == projectId) {
+      if (layout == 'sm') return TablesEditable();
       return TablesEditable();
     }
-    return TablesNoneditable();
+    if (layout == 'sm') return TablesNoneditable();
+    return LargeLayout(
+      content: TablesNoneditableList(),
+      title: '${'Tables of'.tr} ${project?.getLabel() ?? ''}',
+      bottomNavBar: TablesBottomNavBar(),
+    );
   }
 }
