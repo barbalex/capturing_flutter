2e693dacb3c3575db59414c590a7cac802eda68c
Alexander Gabriel <alex.barbalex@gmail.com>
2021-07-20 15:53:43 +0200

    list hierarchic tables hierarchically

--- lib/screens/tree/nodes/editing.dart
+++ lib/screens/tree/nodes/editing.dart
@@ -6,6 +6,7 @@ import 'package:capturing/models/table.dart';
 import 'package:capturing/models/field.dart';
 import 'package:capturing/store.dart';
 import 'dart:math';
+import 'package:collection/collection.dart';
 
 List<Map> buildNodesEditing() {
   final Isar isar = Get.find<Isar>();
@@ -59,7 +60,73 @@ List<Map> buildNodesEditing() {
           })
       .toList();
 
-  List<Map> nodes = [...projectNodes, ...parentTableNodes];
+  List<Ctable> childTables = isar.ctables
+      .where()
+      .filter()
+      .projectIdEqualTo(editingProject.value)
+      .and()
+      .deletedEqualTo(false)
+      .and()
+      .not()
+      .parentIdIsNull()
+      .sortByOrd()
+      .findAllSync();
+
+  List<Map> tableNodes = [...parentTableNodes];
+  int level = 3;
+  while (childTables.isNotEmpty) {
+    List<Ctable> childrenCopied = [...childTables];
+    childrenCopied.forEach((c) {
+      Map? parentMap = tableNodes.firstWhereOrNull(
+          (s) => s['object'].id == c.parentId && s['level'] == level - 1);
+      if (parentMap != null) {
+        int indexOfParentMap = tableNodes.indexOf(parentMap);
+        if (indexOfParentMap > -1) {
+          int newIndex = indexOfParentMap + 1;
+          if (tableNodes.length > newIndex + 1) {
+            // look if childTables of parent exists
+            // need to sort childTables too
+            bool goOn = true;
+            while (goOn) {
+              Map nextMap = tableNodes[newIndex];
+              if (nextMap['level'] == parentMap['level'] + 1) {
+                // sort childTables by ord
+                if (nextMap['object'].ord < c.ord) {
+                  newIndex++;
+                } else {
+                  goOn = false;
+                }
+              } else {
+                goOn = false;
+              }
+            }
+          }
+          tableNodes.insert(
+            newIndex,
+            {
+              'object': c,
+              'url': [
+                '/projects/',
+                c.projectId,
+                '/tables/',
+                c.id,
+              ],
+              'sort': [
+                ...parentMap['sort'],
+                childTables.indexOf(c),
+              ],
+              'level': level,
+              'hasChildren': true, // TODO:
+            },
+          );
+          childTables.removeWhere((child) => child.id == c.id);
+        }
+      }
+    });
+    level++;
+  }
+
+  List<Map> nodes = [...projectNodes, ...tableNodes];
 
   List<String> activeTableIds = [];
   url.asMap().forEach((index, value) {
