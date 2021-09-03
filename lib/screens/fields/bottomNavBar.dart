3ee52f99b64ab9d0b5fd57faced4473e3a8a2601
Alexander Gabriel <alex.barbalex@gmail.com>
2021-09-03 18:49:54 +0200

    optimize

--- lib/screens/fields/bottomNavBar.dart
+++ lib/screens/fields/bottomNavBar.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:capturing/store.dart';
+import 'package:isar/isar.dart';
+import 'package:capturing/isar.g.dart';
+import 'package:capturing/models/table.dart';
 
 class FieldsBottomNavBar extends StatelessWidget {
+  final Isar isar = Get.find<Isar>();
   final String projectId = activeProjectId ?? '';
   final String? tableId = url.length > 1 ? url[url.length - 2] : null;
 
@@ -44,7 +48,10 @@ class FieldsBottomNavBar extends StatelessWidget {
             break;
           case 2:
             if (tableId == null) break;
-            url.value = ['/projects/', projectId, '/tables/', tableId ?? ''];
+            Ctable? table =
+                isar.ctables.where().idEqualTo(tableId ?? '').findFirstSync();
+            if (table == null) break;
+            url.value = table.getUrl();
             break;
         }
       },
