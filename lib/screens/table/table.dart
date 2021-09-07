4fe88b19952efc6e7579d50a54cb293c04c1176a
Alexander Gabriel <alex.barbalex@gmail.com>
2021-09-07 12:11:38 +0200

    add table listener

--- lib/screens/table/table.dart
+++ lib/screens/table/table.dart
@@ -11,6 +11,7 @@ import 'package:capturing/components/nameLabel.dart';
 import 'package:collection/collection.dart';
 import 'package:capturing/store.dart';
 import 'package:capturing/screens/table/singleLabel.dart';
+import 'dart:async';
 
 class TableWidget extends StatefulWidget {
   final Ctable table;
@@ -26,6 +27,26 @@ class TableWidget extends StatefulWidget {
 }
 
 class _TableWidgetState extends State<TableWidget> {
+  late StreamSubscription<void> tableListener;
+
+  @override
+  void initState() {
+    super.initState();
+    tableListener = isar.projects
+        .where()
+        .idEqualTo(widget.table.id)
+        .watchLazy()
+        .listen((event) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    tableListener.cancel();
+    super.dispose();
+  }
+
   final Isar isar = Get.find<Isar>();
   final String projectId = activeProjectId ?? '';
 
