06c1807e32dbc97cb26b11c13c28eacbea41d951
Alexander Gabriel <alex.barbalex@gmail.com>
2021-10-03 16:53:41 +0200

    hasura_connect

--- lib/controllers/sync/dbOperations/tileLayer.dart
+++ lib/controllers/sync/dbOperations/tileLayer.dart
@@ -1,54 +1,56 @@
 import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
-import 'package:hasura_connect/hasura_connect.dart';
 import 'package:isar/isar.dart';
 import 'package:capturing/models/dbOperation.dart';
 import 'package:capturing/isar.g.dart';
 import 'package:capturing/controllers/auth.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class TileLayerOperation {
-  HasuraConnect gqlConnect;
   DbOperation operation;
   final Isar isar = Get.find<Isar>();
+  final GraphQLClient graphqlClient = Get.find<GraphQLClient>();
 
-  TileLayerOperation({required this.gqlConnect, required this.operation}) {
+  TileLayerOperation(this.operation) {
     run();
   }
 
   Future<void> run() async {
     try {
-      await gqlConnect.mutation(
-        r'''
+      await graphqlClient.mutate(
+        MutationOptions(
+          document: gql(r'''
             mutation upsertTileLayers($update_columns: [tile_layers_update_column!]!, $object: tile_layers_insert_input!) {
               insert_tile_layers_one(object: $object, on_conflict: {constraint: tile_layers_pkey, update_columns: $update_columns}) {
                 id
               }
             }
-          ''',
-        variables: {
-          'update_columns': [
-            'label',
-            'url_template',
-            'subdomains',
-            'max_zoom',
-            'min_zoom',
-            'opacity',
-            'wms_base_url',
-            'wms_format',
-            'wms_layers',
-            'wms_parameters',
-            'wms_request',
-            'wms_service',
-            'wms_styles',
-            'wms_transparent',
-            'wms_version',
-            'client_rev_at',
-            'client_rev_by',
-            'server_rev_at',
-            'deleted',
-          ],
-          'object': operation.getData()
-        },
+          '''),
+          variables: {
+            'update_columns': [
+              'label',
+              'url_template',
+              'subdomains',
+              'max_zoom',
+              'min_zoom',
+              'opacity',
+              'wms_base_url',
+              'wms_format',
+              'wms_layers',
+              'wms_parameters',
+              'wms_request',
+              'wms_service',
+              'wms_styles',
+              'wms_transparent',
+              'wms_version',
+              'client_rev_at',
+              'client_rev_by',
+              'server_rev_at',
+              'deleted',
+            ],
+            'object': operation.getData()
+          },
+        ),
       );
     } catch (e) {
       print(e);
