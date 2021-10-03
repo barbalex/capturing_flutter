e951c698aaa9e21d7b13fca369077cfc9f178859
Alexander Gabriel <alex.barbalex@gmail.com>
2021-10-03 16:54:02 +0200

    hasura_connect

--- lib/controllers/sync/dbOperations/project.dart
+++ lib/controllers/sync/dbOperations/project.dart
@@ -1,42 +1,44 @@
 import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
-import 'package:hasura_connect/hasura_connect.dart';
 import 'package:isar/isar.dart';
 import 'package:capturing/models/dbOperation.dart';
 import 'package:capturing/isar.g.dart';
 import 'package:capturing/controllers/auth.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class ProjectOperation {
-  HasuraConnect gqlConnect;
   DbOperation operation;
   final Isar isar = Get.find<Isar>();
+  final GraphQLClient graphqlClient = Get.find<GraphQLClient>();
 
-  ProjectOperation({required this.gqlConnect, required this.operation}) {
+  ProjectOperation(this.operation) {
     run();
   }
 
   Future<void> run() async {
     try {
-      await gqlConnect.mutation(
-        r'''
+      await graphqlClient.mutate(
+        MutationOptions(
+          document: gql(r'''
             mutation upsertProjects($update_columns: [projects_update_column!]!, $object: projects_insert_input!) {
               insert_projects_one(object: $object, on_conflict: {constraint: projects_pkey, update_columns: $update_columns}) {
                 id
               }
             }
-          ''',
-        variables: {
-          'update_columns': [
-            'account_id',
-            'client_rev_at',
-            'client_rev_by',
-            'deleted',
-            'label',
-            'name',
-            'crs'
-          ],
-          'object': operation.getData()
-        },
+          '''),
+          variables: {
+            'update_columns': [
+              'account_id',
+              'client_rev_at',
+              'client_rev_by',
+              'deleted',
+              'label',
+              'name',
+              'crs'
+            ],
+            'object': operation.getData()
+          },
+        ),
       );
     } catch (e) {
       print(e.toString());
