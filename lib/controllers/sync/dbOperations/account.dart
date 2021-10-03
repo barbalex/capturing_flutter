711a30ae7aad4a188d7a9455e2620422540eea2b
Alexander Gabriel <alex.barbalex@gmail.com>
2021-10-03 16:53:53 +0200

    hasura_connect

--- lib/controllers/sync/dbOperations/account.dart
+++ lib/controllers/sync/dbOperations/account.dart
@@ -1,39 +1,41 @@
 import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
-import 'package:hasura_connect/hasura_connect.dart';
 import 'package:isar/isar.dart';
 import 'package:capturing/models/dbOperation.dart';
 import 'package:capturing/isar.g.dart';
 import 'package:capturing/controllers/auth.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class AccountOperation {
-  HasuraConnect gqlConnect;
   DbOperation operation;
   final Isar isar = Get.find<Isar>();
+  final GraphQLClient graphqlClient = Get.find<GraphQLClient>();
 
-  AccountOperation({required this.gqlConnect, required this.operation}) {
+  AccountOperation(this.operation) {
     run();
   }
 
   Future<void> run() async {
     try {
-      await gqlConnect.mutation(
-        r'''
+      await graphqlClient.mutate(
+        MutationOptions(
+          document: gql(r'''
             mutation upsertAccounts($update_columns: [accounts_update_column!]!, $object: accounts_insert_input!) {
               insert_accounts_one(object: $object, on_conflict: {constraint: accounts_pkey, update_columns: $update_columns}) {
                 id
               }
             }
-          ''',
-        variables: {
-          'update_columns': [
-            'service_id',
-            'client_rev_at',
-            'client_rev_by',
-            'deleted'
-          ],
-          'object': operation.getData()
-        },
+          '''),
+          variables: {
+            'update_columns': [
+              'service_id',
+              'client_rev_at',
+              'client_rev_by',
+              'deleted'
+            ],
+            'object': operation.getData()
+          },
+        ),
       );
     } catch (e) {
       print(e);
