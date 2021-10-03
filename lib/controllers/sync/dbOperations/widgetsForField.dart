a30bcea7ddf73d310cb42bcfe29df077a037cedd
Alexander Gabriel <alex.barbalex@gmail.com>
2021-10-03 16:53:31 +0200

    hasura_connect

--- lib/controllers/sync/dbOperations/widgetsForField.dart
+++ lib/controllers/sync/dbOperations/widgetsForField.dart
@@ -1,39 +1,40 @@
 import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
-import 'package:hasura_connect/hasura_connect.dart';
 import 'package:isar/isar.dart';
 import 'package:capturing/models/dbOperation.dart';
 import 'package:capturing/isar.g.dart';
 import 'package:capturing/controllers/auth.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class WidgetsForFieldOperation {
-  HasuraConnect gqlConnect;
   DbOperation operation;
   final Isar isar = Get.find<Isar>();
+  final GraphQLClient graphqlClient = Get.find<GraphQLClient>();
 
-  WidgetsForFieldOperation(
-      {required this.gqlConnect, required this.operation}) {
+  WidgetsForFieldOperation(this.operation) {
     run();
   }
 
   Future<void> run() async {
     try {
-      await gqlConnect.mutation(
-        r'''
+      await graphqlClient.mutate(
+        MutationOptions(
+          document: gql(r'''
             mutation upsertWidgetsForFields($update_columns: [rel_types_update_column!]!, $object: rel_types_insert_input!) {
               insert_rel_types_one(object: $object, on_conflict: {constraint: rel_types_pkey, update_columns: $update_columns}) {
                 id
               }
             }
-          ''',
-        variables: {
-          'update_columns': [
-            'widget_value',
-            'field_value',
-            'deleted',
-          ],
-          'object': operation.getData()
-        },
+          '''),
+          variables: {
+            'update_columns': [
+              'widget_value',
+              'field_value',
+              'deleted',
+            ],
+            'object': operation.getData()
+          },
+        ),
       );
     } catch (e) {
       print(e);
