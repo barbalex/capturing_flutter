32b7ecca7f3928d4645899e5fd56442fed28090d
Alexander Gabriel <alex.barbalex@gmail.com>
2021-10-03 16:54:36 +0200

    hasura_connect

--- lib/controllers/sync/dbOperations/table.dart
+++ lib/controllers/sync/dbOperations/table.dart
@@ -1,48 +1,50 @@
 import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
-import 'package:hasura_connect/hasura_connect.dart';
 import 'package:isar/isar.dart';
 import 'package:capturing/models/dbOperation.dart';
 import 'package:capturing/isar.g.dart';
 import 'package:capturing/controllers/auth.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class TableOperation {
-  HasuraConnect gqlConnect;
   DbOperation operation;
   final Isar isar = Get.find<Isar>();
+  final GraphQLClient graphqlClient = Get.find<GraphQLClient>();
 
-  TableOperation({required this.gqlConnect, required this.operation}) {
+  TableOperation(this.operation) {
     run();
   }
 
   Future<void> run() async {
     try {
-      await gqlConnect.mutation(
-        r'''
+      await graphqlClient.mutate(
+        MutationOptions(
+          document: gql(r'''
             mutation upsertTables($update_columns: [tables_update_column!]!, $object: tables_insert_input!) {
               insert_tables_one(object: $object, on_conflict: {constraint: tables_pkey, update_columns: $update_columns}) {
                 id
               }
             }
-          ''',
-        variables: {
-          'update_columns': [
-            'name',
-            'label',
-            'single_label',
-            'ord',
-            'label_fields',
-            'label_fields_separator',
-            'rel_type',
-            'option_type',
-            'project_id',
-            'parent_id',
-            'client_rev_at',
-            'client_rev_by',
-            'deleted'
-          ],
-          'object': operation.getData()
-        },
+          '''),
+          variables: {
+            'update_columns': [
+              'name',
+              'label',
+              'single_label',
+              'ord',
+              'label_fields',
+              'label_fields_separator',
+              'rel_type',
+              'option_type',
+              'project_id',
+              'parent_id',
+              'client_rev_at',
+              'client_rev_by',
+              'deleted'
+            ],
+            'object': operation.getData()
+          },
+        ),
       );
     } catch (e) {
       print(e);
