b4572d874f26127c8358c9fd2d0726502a045567
Alexander Gabriel <alex.barbalex@gmail.com>
2021-10-03 16:54:07 +0200

    hasura_connect

--- lib/controllers/sync/dbOperations/projectTileLayer.dart
+++ lib/controllers/sync/dbOperations/projectTileLayer.dart
@@ -1,59 +1,60 @@
 import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
-import 'package:hasura_connect/hasura_connect.dart';
 import 'package:isar/isar.dart';
 import 'package:capturing/models/dbOperation.dart';
 import 'package:capturing/isar.g.dart';
 import 'package:capturing/controllers/auth.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class ProjectTileLayerOperation {
-  HasuraConnect gqlConnect;
   DbOperation operation;
   final Isar isar = Get.find<Isar>();
+  final GraphQLClient graphqlClient = Get.find<GraphQLClient>();
 
-  ProjectTileLayerOperation(
-      {required this.gqlConnect, required this.operation}) {
+  ProjectTileLayerOperation(this.operation) {
     run();
   }
 
   Future<void> run() async {
     try {
-      await gqlConnect.mutation(
-        r'''
+      await graphqlClient.mutate(
+        MutationOptions(
+          document: gql(r'''
             mutation upsertProjectTileLayers($update_columns: [project_tile_layers_update_column!]!, $object: project_tile_layers_insert_input!) {
               insert_project_tile_layers_one(object: $object, on_conflict: {constraint: project_tile_layers_pkey, update_columns: $update_columns}) {
                 id
               }
             }
-          ''',
-        variables: {
-          'update_columns': [
-            'project_id',
-            'label',
-            'ord',
-            'active',
-            'url_template',
-            'subdomains',
-            'max_zoom',
-            'min_zoom',
-            'opacity',
-            'wms_base_url',
-            'wms_format',
-            'wms_layers',
-            'wms_parameters',
-            'wms_request',
-            'wms_service',
-            'wms_styles',
-            'wms_transparent',
-            'wms_version',
-            'client_rev_at',
-            'client_rev_by',
-            'server_rev_at',
-            'deleted',
-            'active',
-          ],
-          'object': operation.getData()
-        },
+          '''),
+          variables: {
+            'update_columns': [
+              'project_id',
+              'label',
+              'ord',
+              'active',
+              'url_template',
+              'subdomains',
+              'max_zoom',
+              'min_zoom',
+              'opacity',
+              'wms_base_url',
+              'wms_format',
+              'wms_layers',
+              'wms_parameters',
+              'wms_request',
+              'wms_service',
+              'wms_styles',
+              'wms_transparent',
+              'wms_version',
+              'client_rev_at',
+              'client_rev_by',
+              'server_rev_at',
+              'deleted',
+              'active',
+            ],
+            'object': operation.getData()
+          },
+        ),
       );
     } catch (e) {
       print(e);
