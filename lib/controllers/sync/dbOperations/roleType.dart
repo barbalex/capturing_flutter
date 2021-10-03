b81bb476b4e93a3eca20d514f2e53df73f5e2b52
Alexander Gabriel <alex.barbalex@gmail.com>
2021-10-03 16:54:29 +0200

    hasura_connect

--- lib/controllers/sync/dbOperations/roleType.dart
+++ lib/controllers/sync/dbOperations/roleType.dart
@@ -1,39 +1,41 @@
 import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
-import 'package:hasura_connect/hasura_connect.dart';
 import 'package:isar/isar.dart';
 import 'package:capturing/models/dbOperation.dart';
 import 'package:capturing/isar.g.dart';
 import 'package:capturing/controllers/auth.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class RoleTypeOperation {
-  HasuraConnect gqlConnect;
   DbOperation operation;
   final Isar isar = Get.find<Isar>();
+  final GraphQLClient graphqlClient = Get.find<GraphQLClient>();
 
-  RoleTypeOperation({required this.gqlConnect, required this.operation}) {
+  RoleTypeOperation(this.operation) {
     run();
   }
 
   Future<void> run() async {
     try {
-      await gqlConnect.mutation(
-        r'''
+      await graphqlClient.mutate(
+        MutationOptions(
+          document: gql(r'''
             mutation upsertRoleTypes($update_columns: [role_types_update_column!]!, $object: role_types_insert_input!) {
               insert_role_types_one(object: $object, on_conflict: {constraint: role_types_pkey, update_columns: $update_columns}) {
                 id
               }
             }
-          ''',
-        variables: {
-          'update_columns': [
-            'value',
-            'sort',
-            'comment',
-            'deleted',
-          ],
-          'object': operation.getData()
-        },
+          '''),
+          variables: {
+            'update_columns': [
+              'value',
+              'sort',
+              'comment',
+              'deleted',
+            ],
+            'object': operation.getData()
+          },
+        ),
       );
     } catch (e) {
       print(e);
