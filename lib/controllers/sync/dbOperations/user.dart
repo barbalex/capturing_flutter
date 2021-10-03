36fc1f6d32cb40bc929c4177ad50e7de29b2464a
Alexander Gabriel <alex.barbalex@gmail.com>
2021-10-03 16:53:36 +0200

    hasura_connect

--- lib/controllers/sync/dbOperations/user.dart
+++ lib/controllers/sync/dbOperations/user.dart
@@ -1,42 +1,44 @@
 import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
-import 'package:hasura_connect/hasura_connect.dart';
 import 'package:isar/isar.dart';
 import 'package:capturing/models/dbOperation.dart';
 import 'package:capturing/isar.g.dart';
 import 'package:capturing/controllers/auth.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class UserOperation {
-  HasuraConnect gqlConnect;
   DbOperation operation;
   final Isar isar = Get.find<Isar>();
+  final GraphQLClient graphqlClient = Get.find<GraphQLClient>();
 
-  UserOperation({required this.gqlConnect, required this.operation}) {
+  UserOperation(this.operation) {
     run();
   }
 
   Future<void> run() async {
     try {
-      await gqlConnect.mutation(
-        r'''
+      await graphqlClient.mutate(
+        MutationOptions(
+          document: gql(r'''
             mutation upsertUsers($update_columns: [users_update_column!]!, $object: users_insert_input!) {
               insert_users_one(object: $object, on_conflict: {constraint: users_pkey, update_columns: $update_columns}) {
                 id
               }
             }
-          ''',
-        variables: {
-          'update_columns': [
-            'name',
-            'email',
-            'account_id',
-            'auth_id',
-            'client_rev_at',
-            'client_rev_by',
-            'deleted'
-          ],
-          'object': operation.getData()
-        },
+          '''),
+          variables: {
+            'update_columns': [
+              'name',
+              'email',
+              'account_id',
+              'auth_id',
+              'client_rev_at',
+              'client_rev_by',
+              'deleted'
+            ],
+            'object': operation.getData()
+          },
+        ),
       );
     } catch (e) {
       print(e);
