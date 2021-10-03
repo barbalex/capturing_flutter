576548aff8e366b82aabbd9b324acfff2295e410
Alexander Gabriel <alex.barbalex@gmail.com>
2021-10-03 16:53:44 +0200

    hasura_connect

--- lib/controllers/sync/dbOperations/field.dart
+++ lib/controllers/sync/dbOperations/field.dart
@@ -1,47 +1,49 @@
 import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
-import 'package:hasura_connect/hasura_connect.dart';
 import 'package:isar/isar.dart';
 import 'package:capturing/models/dbOperation.dart';
 import 'package:capturing/isar.g.dart';
 import 'package:capturing/controllers/auth.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class FieldOperation {
-  HasuraConnect gqlConnect;
   DbOperation operation;
   final Isar isar = Get.find<Isar>();
+  final GraphQLClient graphqlClient = Get.find<GraphQLClient>();
 
-  FieldOperation({required this.gqlConnect, required this.operation}) {
+  FieldOperation(this.operation) {
     run();
   }
 
   Future<void> run() async {
     try {
-      await gqlConnect.mutation(
-        r'''
+      await graphqlClient.mutate(
+        MutationOptions(
+          document: gql(r'''
             mutation upsertFields($update_columns: [fields_update_column!]!, $object: fields_insert_input!) {
               insert_fields_one(object: $object, on_conflict: {constraint: fields_pkey, update_columns: $update_columns}) {
                 id
               }
             }
-          ''',
-        variables: {
-          'update_columns': [
-            'table_id',
-            'name',
-            'label',
-            'ord',
-            'is_internal_id',
-            'field_type',
-            'widget_type',
-            'options_table',
-            'standard_value',
-            'client_rev_at',
-            'client_rev_by',
-            'deleted'
-          ],
-          'object': operation.getData()
-        },
+          '''),
+          variables: {
+            'update_columns': [
+              'table_id',
+              'name',
+              'label',
+              'ord',
+              'is_internal_id',
+              'field_type',
+              'widget_type',
+              'options_table',
+              'standard_value',
+              'client_rev_at',
+              'client_rev_by',
+              'deleted'
+            ],
+            'object': operation.getData()
+          },
+        ),
       );
     } catch (e) {
       print(e);
