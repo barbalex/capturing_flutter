b3f370d8f344c3789c22c477cce2c8cfd19bfd37
Alexander Gabriel <alex.barbalex@gmail.com>
2021-10-03 16:53:27 +0200

    hasura_connect

--- lib/controllers/sync/dbOperations/widgetType.dart
+++ lib/controllers/sync/dbOperations/widgetType.dart
@@ -1,40 +1,42 @@
 import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
-import 'package:hasura_connect/hasura_connect.dart';
 import 'package:isar/isar.dart';
 import 'package:capturing/models/dbOperation.dart';
 import 'package:capturing/isar.g.dart';
 import 'package:capturing/controllers/auth.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class WidgetTypeOperation {
-  HasuraConnect gqlConnect;
   DbOperation operation;
   final Isar isar = Get.find<Isar>();
+  final GraphQLClient graphqlClient = Get.find<GraphQLClient>();
 
-  WidgetTypeOperation({required this.gqlConnect, required this.operation}) {
+  WidgetTypeOperation(this.operation) {
     run();
   }
 
   Future<void> run() async {
     try {
-      await gqlConnect.mutation(
-        r'''
+      await graphqlClient.mutate(
+        MutationOptions(
+          document: gql(r'''
             mutation upsertWidgetTypes($update_columns: [widget_types_update_column!]!, $object: widget_types_insert_input!) {
               insert_widget_types_one(object: $object, on_conflict: {constraint: widget_types_pkey, update_columns: $update_columns}) {
                 id
               }
             }
-          ''',
-        variables: {
-          'update_columns': [
-            'value',
-            'needs_list',
-            'sort',
-            'comment',
-            'deleted',
-          ],
-          'object': operation.getData()
-        },
+          '''),
+          variables: {
+            'update_columns': [
+              'value',
+              'needs_list',
+              'sort',
+              'comment',
+              'deleted',
+            ],
+            'object': operation.getData()
+          },
+        ),
       );
     } catch (e) {
       print(e);
