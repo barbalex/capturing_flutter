aa8ae9aec75efcf19f5710dfe6f604cba7b8b828
Alexander Gabriel <alex.barbalex@gmail.com>
2022-01-23 15:34:08 +0100

    correct

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,7 +5,7 @@ bool get isDev => kReleaseMode ? false : true;
 // magic address for local dev, see: https://github.com/Flutterando/hasura_connect/issues/7#issuecomment-539441669
 // String get graphQlUri =>
 //     isDev ? 'http://10.0.2.2:8080/v1/graphql' : 'https://api.capturing.app';
-String get graphQlUri => 'https://api.capturing.app';
+String get graphQlUri => 'https://api.capturing.app/v1/graphql';
 
 String authUri = 'https://auth.capturing.app';
 
