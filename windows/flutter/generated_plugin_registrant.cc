d0c80661b73ccd2314eebcef8dd9bc1ec11a86cb
Alexander Gabriel <alex.barbalex@gmail.com>
2022-03-09 17:44:55 +0100

    update deps

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,6 +7,7 @@
 #include "generated_plugin_registrant.h"
 
 #include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
+#include <desktop_webview_auth/desktop_webview_auth_plugin.h>
 #include <geolocator_windows/geolocator_windows.h>
 #include <isar_flutter_libs/isar_flutter_libs_plugin.h>
 #include <native_pdf_renderer/native_pdf_renderer_plugin.h>
@@ -14,6 +15,8 @@
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   ConnectivityPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
+  DesktopWebviewAuthPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DesktopWebviewAuthPlugin"));
   GeolocatorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("GeolocatorWindows"));
   IsarFlutterLibsPluginRegisterWithRegistrar(
